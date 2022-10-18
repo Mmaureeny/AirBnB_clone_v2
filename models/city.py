8be889fd6a18c936c9c5292812ecd0401e296d5f
Mmaureeny <muthonimaureen4@outlook.com>
2022-10-18 06:14:49 +0000

    indentetion corrections

--- models/city.py
+++ models/city.py
@@ -11,7 +11,7 @@ class City(BaseModel):
         state_id: The state id
         name: input name
     """
-   __tablename__ = 'cities'
+    __tablename__ = 'cities'
     state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
     name = Column(String(128), nullable=False)
     # One2many relationship to places
--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -8,6 +8,12 @@ from flask import render_template
 app = Flask(__name__)
 
 
+
+@app.teardown_appcontext
+def teardown(self):
+    """Removes the current SQLAlchemy Session"""
+    storage.close()
+
 @app.route('/states_list', strict_slashes=False)
 def states():
     """Returns a rendered html template
@@ -16,11 +22,5 @@ def states():
     return render_template('7-states_list.html',
                            states=storage.all('State').values())
 
-
-@app.teardown_appcontext
-def teardown(self):
-    """Removes the current SQLAlchemy Session"""
-    storage.close()
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000)
