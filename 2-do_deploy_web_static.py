0f09941c522c86c69367b03352e325a13994d798
Mmaureeny <muthonimaureen4@outlook.com>
2022-10-28 18:14:41 +0000

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,12 +3,37 @@
 archive to the web servers
 """
 from os.path import exists
-from fabric.api import put, run, env
+from fabric.api import put, run, env, local, runs_once
+from datetime import datetime
 
 
 env.hosts = ["3.239.58.144", "100.24.126.136"]
 env.user = 'ubuntu'
 """The list of host server IP addresses."""
+
+
+@runs_once
+def do_pack():
+    """Archives the static files."""
+    if not os.path.isdir("versions"):
+        os.mkdir("versions")
+    cur_time = datetime.now()
+    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+        cur_time.year,
+        cur_time.month,
+        cur_time.day,
+        cur_time.hour,
+        cur_time.minute,
+        cur_time.second
+    )
+    try:
+        print("Packing web_static to {}".format(output))
+        local("tar -cvzf {} web_static".format(output))
+        archize_size = os.stat(output).st_size
+        print("web_static packed: {} -> {} Bytes".format(output, archize_size))
+    except Exception:
+        output = None
+    return output
 
 
 def do_deploy(archive_path):
